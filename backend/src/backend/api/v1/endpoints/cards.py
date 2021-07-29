a5f9d570b0f51215457bb5395a41bbec8681cb1c
Marcio <msk@certi.org.br>
2021-07-29 00:44:41 -0300

    [BACKEND] add get route to retrieve all cards from table

--- backend/src/backend/api/v1/endpoints/cards.py
+++ backend/src/backend/api/v1/endpoints/cards.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
@@ -24,3 +26,28 @@ def create(details: CreateCard, db: Session = Depends(get_db)) -> HTMLResponse:
         )
 
     return HTMLResponse(content='Added new card {}'.format(new_card.value), status_code=201)
+
+
+@router.get("/")
+def retrieve(db: Session = Depends(get_db)) -> HTMLResponse:
+    cards_list = []
+    try:
+        cards = db.query(Card).all()
+        for card in cards:
+            cards_list.append(row2dict(card))
+        print('$$$$$$ list: {}'.format(cards_list))
+        db.commit()
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Could not retrieve from cards table',
+        )
+
+    return HTMLResponse(content=json.dumps(cards_list), status_code=201)
+
+
+def row2dict(card):
+    d = {}
+    for column in card.__table__.columns:
+        d[column.name] = str(getattr(card, column.name))
+    return d
