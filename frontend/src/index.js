f7a5f85832eeffd7ef640e90542c1e0841d4ac54
Marcio <msk@certi.org.br>
2021-07-30 01:19:12 -0300

    clean up frontend directory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "./index.css";
 
 const retrieveCards = () => {
   return new Promise((resolve, reject) => {
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
-  app.use(createProxyMiddleware("/cards", { target: "http://backend:8000" }));
+  app.use(createProxyMiddleware("/cardsj", { target: "http://backend:8000" }));
 };
